74385a7cb9eb51580433ce38beaae15b109f1a7f
DonaldGj <donaldgjegji1@gmail.com>
2025-10-18 21:17:01 +0200

    Fix style

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""A script that performs element-wise addition, subtraction, multiplication, and division"""
+"""A script that performs element-wise addition, subtraction etc."""
 
 
 def np_elementwise(mat1, mat2):
-    """A function that performs element-wise addition, subtraction, multiplication, and division"""
+    """A function that performs element-wise addition, subtraction etc."""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
