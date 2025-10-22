9d61f22f254d9e625e917f9bcf9d115eb005acd8
DonaldGj <donaldgjegji1@gmail.com>
2025-10-22 21:48:37 +0200

    Add type validation

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,12 +6,9 @@ def poly_derivative(poly):
     """A function that calculates the derivative of a polynomial"""
     if not isinstance(poly, list) or len(poly) == 0:
         return None
-    
     derivative = []
     for power in range(1, len(poly)):
         derivative.append(power * poly[power])
-    
     if all(coeff == 0 for coeff in derivative):
         return [0]
-    
     return derivative
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,4 +4,6 @@
 
 def summation_i_squared(n):
     """A function that sums up i squared"""
+    if not isinstance(n, int) or n < 1:
+        return None
     return n**3/3 + n**2/2 + n/6
