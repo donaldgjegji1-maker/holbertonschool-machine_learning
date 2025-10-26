326d3a2a12facd32961f93a9dc52193f181bf421
DonaldGj <donaldgjegji1@gmail.com>
2025-10-26 20:25:57 +0100

    Format code

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -3,12 +3,11 @@
 
 
 class Binomial:
-    """Class representing a Binomial distribution"""
+    """Class with constructor for a Binomial distribution"""
 
     def __init__(self, data=None, n=1, p=0.5):
         """
-        Initializes a Binomial distribution.
-        If data is provided, estimates n and p from the data.
+        A class that represents a Binomial distribution
         """
         if data is None:
             if n <= 0:
@@ -22,24 +21,12 @@ class Binomial:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
             mean = sum(data) / len(data)
             variance = sum((x - mean) ** 2 for x in data) / len(data)
-
-            p_est = 1 - (variance / mean)
-            n_est = mean / p_est
-
-            self.n = round(n_est)
+            p_first = 1 - (variance / mean)
+            n_round = mean / p_first
+            self.n = round(n_round)
             self.p = mean / self.n
-
-    def factorial(self, x):
-        """Computes factorial of x manually (no imports)."""
-        if x == 0 or x == 1:
-            return 1
-        f = 1
-        for i in range(2, x + 1):
-            f *= i
-        return f
 
     def pmf(self, k):
         """
@@ -51,26 +38,36 @@ class Binomial:
         if k < 0 or k > self.n:
             return 0
 
-        # Combination: C(n, k) = n! / (k! * (n - k)!)
-        comb = self.factorial(self.n) / (
-            self.factorial(k) * self.factorial(self.n - k)
-        )
+        def factorial(x):
+            """Function for factorial"""
+            if x == 0 or x == 1:
+                return 1
+            f = 1
+            for i in range(2, x + 1):
+                f *= i
+            return f
 
+        # Combination: C(n, k) = n! / (k! * (n - k)!)
+        comb = factorial(self.n) / (factorial(k) * factorial(self.n - k))
         # PMF: C(n, k) * p^k * (1 - p)^(n - k)
         return comb * (self.p ** k) * ((1 - self.p) ** (self.n - k))
 
     def cdf(self, k):
         """
-        Calculate the cumulative distribution function (CDF)
-        for a given number of successes k.
+        Calculate the cumulative distribution function (CDF) for a given
+        number of successes k.
+
+        Args:
+            k (int or float): Number of successes
+
+        Returns:
+            float: CDF value for k
         """
-        if not isinstance(k, int):
-            k = int(k)
+        k = int(k)
         if k < 0:
             return 0
         if k >= self.n:
             return 1
-
         total = 0
         for i in range(0, k + 1):
             total += self.pmf(i)
