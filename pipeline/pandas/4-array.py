0a4f783fdd1ed84d5eb0eabf1de19578cea383e4
DonaldGj <donaldgjegji1@gmail.com>
2025-11-01 21:47:53 +0100

    Remove not allowed imports (2)

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """A script that takes a pd.DataFrame as input and performs operations"""
 
-import pandas as pd
-
 
 def array(df):
     """A function that takes a pd.DataFrame as input and performs operations"""
