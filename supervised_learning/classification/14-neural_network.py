2f8e73512e156e7eac2d187fa10c2bfa0aab0adf
DonaldGj <donaldgjegji1@gmail.com>
2026-01-19 20:30:27 +0100

    Fix

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module that that defines a neural network with one hidden layer
+Module that defines a neural network with one hidden layer
 performing binary classification
 """
 
@@ -32,8 +32,8 @@ class NeuralNetwork:
 
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
-
         self.__A1 = 0
+
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
@@ -71,59 +71,85 @@ class NeuralNetwork:
     def forward_prop(self, X):
         """
         Calculates the forward propagation of the neural network
+
+        X: numpy.ndarray with shape (nx, m) containing input data
+        Returns: the private attributes __A1 and __A2
         """
         Z1 = np.matmul(self.__W1, X) + self.__b1
         self.__A1 = 1 / (1 + np.exp(-Z1))
 
         Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
         self.__A2 = 1 / (1 + np.exp(-Z2))
+
         return self.__A1, self.__A2
 
     def cost(self, Y, A):
         """
         Calculates the cost of the model using logistic regression
+
+        Y: numpy.ndarray with shape (1, m) containing correct labels
+        A: numpy.ndarray with shape (1, m) containing activated output
+        Returns: the cost
         """
         m = Y.shape[1]
-        c = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
-        return c
+        cost = -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+        return cost
 
     def evaluate(self, X, Y):
         """
         Evaluates the neural network's predictions
+
+        X: numpy.ndarray with shape (nx, m) containing input data
+        Y: numpy.ndarray with shape (1, m) containing correct labels
+        Returns: the neuron's prediction and the cost
         """
         A1, A2 = self.forward_prop(X)
         cost = self.cost(Y, A2)
-        result = np.where(A2 >= 0.5, 1, 0)
-        return result, cost
+        prediction = np.where(A2 >= 0.5, 1, 0)
+        return prediction, cost
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """
         Calculates one pass of gradient descent on the neural network
+
+        X: numpy.ndarray with shape (nx, m) containing input data
+        Y: numpy.ndarray with shape (1, m) containing correct labels
+        A1: output of the hidden layer
+        A2: predicted output
+        alpha: learning rate
         """
         m = Y.shape[1]
+
+        # Backpropagation for output layer
         dZ2 = A2 - Y
-        dW2 = (1/m) * np.matmul(dZ2, A1.T)
-        db2 = (1/m) * np.sum(dZ2)
+        dW2 = np.matmul(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
 
-        dZ1 = np.matmul(self.__W2.T, dZ2) * A1 * (1 - A1)
-        dW1 = (1/m) * np.matmul(dZ1, X.T)
-        db1 = (1/m) * np.sum(dZ1, axis=1, keepdims=True)
+        # Backpropagation for hidden layer
+        dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = np.matmul(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
 
-        self.__W1 = self.__W1 - alpha * dW1
-        self.__b1 = self.__b1 - alpha * db1
-        self.__W2 = self.__W2 - alpha * dW2
-        self.__b2 = self.__b2 - alpha * db2
+        # Update weights and biases
+        self.__W2 = self.__W2 - (alpha * dW2)
+        self.__b2 = self.__b2 - (alpha * db2)
+        self.__W1 = self.__W1 - (alpha * dW1)
+        self.__b1 = self.__b1 - (alpha * db1)
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """
-        Train the neural network
-        """
+        Trains the neural network
 
+        X: numpy.ndarray with shape (nx, m) containing input data
+        Y: numpy.ndarray with shape (1, m) containing correct labels
+        iterations: number of iterations to train over
+        alpha: learning rate
+        Returns: the evaluation of the training data after iterations
+        """
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha <= 0:
