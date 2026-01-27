11cfc9203eb95593bd7aa97e72c32a2e949c43c8
DonaldGj <donaldgjegji1@gmail.com>
2026-01-27 21:49:49 +0100

    Rename import (2)

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -2,35 +2,35 @@
 """
 Build a neural network with Keras
 """
-import tensorflow as K
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Builds a neural network with the Keras library
     """
-    model = K.keras.Sequential()
+    model = K.Sequential()
 
-    model.add(K.keras.layers.Dense(
+    model.add(K.layers.Dense(
         units=layers[0],
         activation=activations[0],
-        kernel_regularizer=K.keras.regularizers.l2(lambtha),
+        kernel_regularizer=K.regularizers.l2(lambtha),
         input_shape=(nx,)
     ))
-    model.add(K.keras.layers.Dropout(rate=1 - keep_prob))
+    model.add(K.layers.Dropout(rate=1 - keep_prob))
 
     for i in range(1, len(layers) - 1):
-        model.add(K.keras.layers.Dense(
+        model.add(K.layers.Dense(
             units=layers[i],
             activation=activations[i],
-            kernel_regularizer=K.keras.regularizers.l2(lambtha)
+            kernel_regularizer=K.regularizers.l2(lambtha)
         ))
-        model.add(K.keras.layers.Dropout(rate=1 - keep_prob))
+        model.add(K.layers.Dropout(rate=1 - keep_prob))
 
-    model.add(K.keras.layers.Dense(
+    model.add(K.layers.Dense(
         units=layers[-1],
         activation=activations[-1],
-        kernel_regularizer=K.keras.regularizers.l2(lambtha)
+        kernel_regularizer=K.regularizers.l2(lambtha)
     ))
 
     return model
