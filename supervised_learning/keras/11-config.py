2e08d4ba828b4ca34a5a17c8c5ad78bbf19132b7
DonaldGj <donaldgjegji1@gmail.com>
2026-01-28 20:27:56 +0100

    Remove import

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -3,17 +3,14 @@
 Functions for saving and loading Keras model configuration
 """
 import tensorflow.keras as K
-import json
 
 
 def save_config(network, filename):
     """
     Saves a model's configuration in JSON format
     """
-    config = network.get_config()
-
     with open(filename, 'w') as f:
-        json.dump(config, f)
+        f.write(network.to_json())
 
 
 def load_config(filename):
@@ -21,8 +18,4 @@ def load_config(filename):
     Loads a model with a specific configuration
     """
     with open(filename, 'r') as f:
-        config = json.load(f)
-
-    model = K.models.model_from_config(config)
-
-    return model
+        return K.models.model_from_json(f.read())
