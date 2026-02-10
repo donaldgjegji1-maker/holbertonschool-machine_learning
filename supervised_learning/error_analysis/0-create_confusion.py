6aa797d245e50275d8de83015dce8c036fefdb99
DonaldGj <donaldgjegji1@gmail.com>
2026-02-10 20:44:38 +0100

    Fix output

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
-"""Create Confusion Matrix""""
+"""
+Confusion Matrix Creation Module
+"""
+
 import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
-    """Creates a confusion matrix"""
-    return np.matmul(labels.T, logits)
+    """
+    Creates a confusion matrix.
+    """
+    # Convert one-hot to class indices
+    true_classes = np.argmax(labels, axis=1)
+    pred_classes = np.argmax(logits, axis=1)
+
+    classes = labels.shape[1]
+    confusion = np.zeros((classes, classes))
+
+    for true, pred in zip(true_classes, pred_classes):
+        confusion[true, pred] += 1
+
+    return confusion
