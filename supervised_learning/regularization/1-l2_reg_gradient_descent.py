51b02634cb1cde73695503f30f88fa2ed4fbd921
DonaldGj <donaldgjegji1@gmail.com>
2026-02-11 19:51:50 +0100

    Fix output

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -12,21 +12,27 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
     m = Y.shape[1]
 
+    # Output layer gradient
     dZ = cache['A' + str(L)] - Y
 
     # Loop through layers in reverse order
     for layer in range(L, 0, -1):
+        # Get previous layer activations
         A_prev = cache['A' + str(layer - 1)]
 
+        # Get current weights
         W = weights['W' + str(layer)]
 
-        dW = np.matmul(dZ, A_prev.T) / m
+        # Calculate gradients with L2 regularization
+        dW = np.matmul(dZ, A_prev.T) + lambtha * W
+        dW = dW / m
+
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
-        dW += (lambtha / m) * W
-
+        # Update weights and biases
         weights['W' + str(layer)] -= alpha * dW
         weights['b' + str(layer)] -= alpha * db
 
+        # Backpropagate to previous layer (if not at first layer)
         if layer > 1:
             dZ = np.matmul(W.T, dZ) * (1 - cache['A' + str(layer - 1)] ** 2)
