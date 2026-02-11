e52185112639823794cbb17e7858c29e6710f684
DonaldGj <donaldgjegji1@gmail.com>
2026-02-11 20:15:29 +0100

    Fix output (3)

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-L2 Regularization Gradient Descent Module
+L2 Regularization Gradient Descent
 """
 import numpy as np
 
@@ -11,27 +11,20 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     with L2 regularization
     """
     m = Y.shape[1]
+    dZ = cache['A' + str(L)] - Y
 
-    # Copy weights to use original values during backpropagation
-    weights_copy = weights.copy()
-
-    dZ = cache[f'A{L}'] - Y
-
-    # Backpropagate through all layers
-    for l in range(L, 0, -1):
-        # Get the activation from previous layer
-        A_prev = cache[f'A{l - 1}']
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
 
         # Calculate gradients with L2 regularization
-        dW = (1/m)*np.matmul(dZ, A_prev.T)+(lambtha/m)*weights_copy[f'W{l}']
-        db = (1/m)*np.sum(dZ, axis=1, keepdims=True)
+        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        # Update weights and biases in place
-        weights[f'W{l}'] -= alpha * dW
-        weights[f'b{l}'] -= alpha * db
+        # Calculate dZ for previous layer before updating weights
+        if i > 1:
+            dZ = np.matmul(W.T, dZ) * (1 - np.power(A_prev, 2))
 
-        # Calculate dZ for previous layer (if not at the first layer)
-        if l > 1:
-            dA = np.matmul(weights_copy[f'W{l}'].T, dZ)
-            # For tanh: derivative is (1 - A^2)
-            dZ = dA * (1 - np.square(A_prev))
+        # Update weights and biases
+        weights['W' + str(i)] = W - alpha * dW
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
