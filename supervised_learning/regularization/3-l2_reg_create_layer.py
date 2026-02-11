55d990b06cb5c5c15fa7a91ff61591b194e4c1ab
DonaldGj <donaldgjegji1@gmail.com>
2026-02-11 20:23:57 +0100

    Fix output (6)

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 """
-L2 Regularization Layer Creation Module for TensorFlow
+Create Layer with L2 Regularization
 """
-
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creates a neural network layer with L2 regularization.
+    Creates a neural network layer in TensorFlow
+    that includes L2 regularization
     """
-    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-
-    # Create L2 regularizer
     regularizer = tf.keras.regularizers.l2(lambtha)
+
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
+    )
 
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
         kernel_initializer=initializer,
-        kernel_regularizer=regularizer,
-        name=f'dense_layer_{n}'
+        kernel_regularizer=regularizer
     )
 
-    output = layer(prev)
-    return output
+    return layer(prev)
