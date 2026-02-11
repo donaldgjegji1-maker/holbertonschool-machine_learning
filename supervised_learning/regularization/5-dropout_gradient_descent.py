51e2c01efd62f2c376dd94ed46a29bca8a75b193
DonaldGj <donaldgjegji1@gmail.com>
2026-02-11 20:28:59 +0100

    Fix output (7)

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python3
 """
-Dropout Gradient Descent Module
+Dropout Gradient Descent
 """
-
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Updates weights of a neural network with Dropout regularization
-    using gradient descent.
+    Updates the weights of a neural network with Dropout regularization
+    using gradient descent
     """
     m = Y.shape[1]
     dZ = cache['A' + str(L)] - Y
 
-    # Loop through layers in reverse order
+    # Backpropagate through all layers
     for layer in range(L, 0, -1):
         A_prev = cache['A' + str(layer - 1)]
 
-        W = weights['W' + str(layer)]
+        dW = (1 / m) * np.matmul(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        dW = np.matmul(dZ, A_prev.T) / m
-        db = np.sum(dZ, axis=1, keepdims=True) / m
+        if layer > 1:
+            W = weights['W' + str(layer)]
+
+            dA = np.matmul(W.T, dZ)
+
+            # Apply dropout mask to dA
+            D = cache['D' + str(layer - 1)]
+            dA = dA * D
+
+            # Scale by keep_prob to maintain expected value
+            dA = dA / keep_prob
+
+            # Apply derivative of tanh: (1 - A^2)
+            dZ = dA * (1 - np.power(A_prev, 2))
 
         # Update weights and biases
         weights['W' + str(layer)] -= alpha * dW
         weights['b' + str(layer)] -= alpha * db
-
-        # Calculate gradient for next layer
-        if layer > 1:
-            # Get dropout mask for previous layer
-            D = cache['D' + str(layer - 1)]
-
-            # Backpropagate through tanh activation
-            dA = np.matmul(W.T, dZ)
-
-            # Apply dropout mask to gradient
-            dA = dA * D
-
-            # Scale gradients to compensate for dropout
-            dA = dA / keep_prob
-            dZ = dA * (1 - cache['A' + str(layer - 1)] ** 2)
