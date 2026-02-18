dad57390aed04552ce8dcd2f6cb2e1286287da95
DonaldGj <donaldgjegji1@gmail.com>
2026-02-18 19:43:38 +0100

    Fix output

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for same convolution on grayscale images
+Function that performs a same convolution on grayscale images.
 """
 import numpy as np
 
@@ -10,26 +10,24 @@ def convolve_grayscale_same(images, kernel):
     Performs a same convolution on grayscale images
     """
     m, h, w = images.shape
-    kh, kw = kernel.shape
+    kh, kw = kernel.Shape
 
-    pad_h = (kh - 1) // 2
-    pad_w = (kw - 1) // 2
+    pad_h = kh // 2
+    pad_w = kw // 2
 
-    pad_top = (kh - 1) // 2
-    pad_bot = (kh - 1) - pad_top
-    pad_left = (kw - 1) // 2
-    pad_right = (kw - 1) - pad_left
+    # Pad images with zeros on all sides
+    padded_images = np.pad(
+        images,
+        pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+        mode='constant',
+        constant_values=0
+    )
 
-    # Pad the images
-    padded_images = np.pad(images,
-                           ((0, 0), (pad_top, pad_bot), (pad_left, pad_right)),
-                           mode='constant', constant_values=0)
-
-    # Initialize output array
     output = np.zeros((m, h, w))
 
     for i in range(h):
         for j in range(w):
-            region = padded_images[:, i:i+kh, j:j+kw]
+            region = padded_images[:, i:i + kh, j:j + kw]
             output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
+
     return output
