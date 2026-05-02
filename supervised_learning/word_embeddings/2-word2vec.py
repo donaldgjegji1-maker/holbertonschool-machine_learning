22cdf19418c25e86a66ac096972cc099ca28488e
DonaldGj <donaldgjegji1@gmail.com>
2026-05-02 22:54:20 +0200

    Fix word2vec

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -3,23 +3,32 @@
 import gensim
 
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates, builds and trains a gensim word2vec model"""
+def word2vec_model(sentences, vector_size=100, min_count=5,
+                   window=5, negative=5, cbow=True,
+                   epochs=5, seed=0, workers=1):
+    """
+    """
+    # Set the training algorithm based on cbow parameter
+    sg = 0 if cbow else 1
+
+    # Create the Word2Vec model
     model = gensim.models.Word2Vec(
+        sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=0 if cbow else 1,
+        sg=sg,
         epochs=epochs,
         seed=seed,
         workers=workers
-    )
+        )
+
+    # prepare the model vocabulary
     model.build_vocab(sentences)
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=model.epochs
-    )
+
+    # Train the model
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
+
     return model
