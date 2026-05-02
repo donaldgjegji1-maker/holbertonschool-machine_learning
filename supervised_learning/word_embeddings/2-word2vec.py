dbc374563336910fa49f53768eb2543d6f360cbf
DonaldGj <donaldgjegji1@gmail.com>
2026-05-02 22:35:05 +0200

    Downgrade gensim

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -7,17 +7,14 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Creates, builds and trains a gensim word2vec model"""
     model = gensim.models.Word2Vec(
-        vector_size=vector_size,
+        size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
         sg=0 if cbow else 1,
-        epochs=epochs,
+        iter=epochs,
         seed=seed,
-        workers=workers,
-        hs=0,
-        alpha=0.025,
-        min_alpha=0.0001,
+        workers=workers
     )
     model.build_vocab(sentences)
     model.train(
