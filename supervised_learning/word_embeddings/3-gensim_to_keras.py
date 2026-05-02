0f5d06b68cb663d8b51f0115220327ba6265cb37
DonaldGj <donaldgjegji1@gmail.com>
2026-05-02 22:46:45 +0200

    Change order

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -5,8 +5,10 @@ import tensorflow as tf
 
 def gensim_to_keras(model):
     """Converts a gensim word2vec model to a trainable keras Embedding layer"""
-    weights = model.wv.vectors
-    vocab_size, vector_size = weights.shape
+    keyed_vectors = model.wv
+    vocab_size = len(keyed_vectors)
+    vector_size = keyed_vectors.vector_size
+    weights = [keyed_vectors[word] for word in keyed_vectors.index_to_key]
     return tf.keras.layers.Embedding(
         input_dim=vocab_size,
         output_dim=vector_size,
