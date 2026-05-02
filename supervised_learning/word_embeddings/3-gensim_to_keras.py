65c0d00d665bda9c9ac0307f18c0a8f171676cb0
DonaldGj <donaldgjegji1@gmail.com>
2026-05-02 22:56:07 +0200

    Fix extra word2vec

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -4,14 +4,12 @@ import tensorflow as tf
 
 
 def gensim_to_keras(model):
-    """Converts a gensim word2vec model to a trainable keras Embedding layer"""
-    keyed_vectors = model.wv
-    vocab_size = len(keyed_vectors)
-    vector_size = keyed_vectors.vector_size
-    weights = [keyed_vectors[word] for word in keyed_vectors.index_to_key]
-    return tf.keras.layers.Embedding(
-        input_dim=vocab_size,
-        output_dim=vector_size,
-        embeddings_initializer=tf.keras.initializers.Constant(weights),
-        trainable=True
-    )
+    """
+    """
+    keys = model.wv
+    weights = keys.vectors
+
+    return tf.keras.layers.Embedding(input_dim=weights.shape[0],
+                                     output_dim=weights.shape[1],
+                                     weights=[weights],
+                                     trainable=True)
